155dbf42da7c790e32f9638491a8f78669f9d10b
joefromct <yeoj@localhost.localdomain>
2014-06-28 17:34:57 -0400

    lucys changes

---
+++ rsync.sh
@@ -0,0 +1,1 @@
+rsync -av --progress yeoj@192.168.1.120:/var/www/webapp ./
--- src/webapp/routes/home.clj
+++ src/webapp/routes/home.clj
@@ -16,10 +16,22 @@
 (defn guest-information [] ( layout/render "guest-information.html" {:dynamic-content (str " guest-information.html content")})  ) 
 (defn contact []           ( layout/render "contact.html"           {:dynamic-content (str " contact.html           content")})  ) 
 
+(defn accommodation []           ( layout/render "accommodation.html"           {:dynamic-content (str " accommodation.html           content")})  ) 
+
+(defn ceremony-reception []           ( layout/render "ceremony-reception.html"           {:dynamic-content (str "ceremony-reception.html           content")})  ) 
+(defn location []           ( layout/render "location.html"           {:dynamic-content (str "location.html           content")})  ) 
+
+
+(defn other-information []           ( layout/render "other-information.html"           {:dynamic-content (str "other-information.html           content")})  ) 
+
 (defroutes home-routes
     ( GET "/" []                  ( home-page))
     ( GET "/our-proposal" []      ( our-proposal))
     ( GET "/wedding-party" []     ( wedding-party))
     ( GET "/guest-information" [] ( guest-information))
-    ( GET "/contact" []           ( contact)))
+    ( GET "/ceremony-reception" [] ( ceremony-reception ))
+    ( GET "/location" [] ( location ))
+    ( GET "/other-information" [] ( other-information ))
+    ( GET "/contact" []           ( contact))
+    ( GET "/accommodation" []           ( accommodation)))
 
